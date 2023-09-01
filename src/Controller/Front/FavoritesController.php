8775796b7932e37f129039669a21080d284f6250
YoanBuscail <yoan.buscail@gmail.com>
2023-09-01 20:07:08 +0200

    ajouter favoris et supprimer un favoris ok

--- src/Controller/Front/FavoritesController.php
+++ src/Controller/Front/FavoritesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +13,64 @@ class FavoritesController extends AbstractController
     /**
      * @Route("/favorites", name="app_favorites")
      */
-    public function index(): Response
+    public function index(Request $request, MovieRepository $movieRepository): Response
     {
-        return $this->render('favorites/index.html.twig');
+        $session = $request->getSession();
+
+        $favoris = $session->get('favoris', []);
+
+        return $this->render('favorites/index.html.twig', [
+        'favoris' => $favoris,
+        'movieRepository' => $movieRepository,
+        ]);
+    }
+    
+    /**
+     * @Route("/favorites/add/{id}", name="app_favorite_add")
+     */
+    public function addToFavorite($id, Request $request): Response
+    {
+        // Récupérer la session
+        $session = $request->getSession();
+
+        // Récupérer le panier de favoris de la session
+        $favoris = $session->get('favoris', []);
+
+        // Ajouter le film à la liste de favoris s'il n'y est pas déjà
+        if (!in_array($id, $favoris)) {
+            $favoris[] = $id;
+        }
+
+        // Mettre à jour le panier de favoris dans la session
+        $session->set('favoris', $favoris);
+        
+        $this->addFlash('add_to_favorite', 'Film ajouté à la liste des favoris ! ');
+
+        return $this->redirectToRoute('app_favorites', ['id' => $id]);
+    }
+    
+    /**
+     * @Route("/favorites/remove/{id}", name="app_favorite_remove")
+     *
+     */
+    public function removeFromFavorite($id, Request $request): Response
+    {
+        $session = $request->getSession();
+
+        $favoris = $session->get('favoris', []);
+
+        // Vérifier si l'ID du film à supprimer est présent dans la liste des favoris
+        $key = array_search($id, $favoris);
+
+        if ($key !== false) {
+            // Si l'ID existe, supprimer de la liste
+            unset($favoris[$key]);
+            
+            // Mettez à jour le panier de favoris dans la session
+            $session->set('favoris', $favoris);
+        }
+
+        // Rediriger vers la page voulue (par exemple, la page de la liste de favoris)
+        return $this->redirectToRoute('app_favorites');
     }
 }
