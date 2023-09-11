a5587e526dbb308ad5e04963336bec145b28c10d
YoanBuscail <yoan.buscail@gmail.com>
2023-09-11 15:33:08 +0200

    retourner les erreurs api en json

---
+++ src/EventSubscriber/ApiExceptionSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ApiExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        //  Je vérifie que la route commence par api, si c'es pas le cas on stop
+        if (strpos($request->getPathInfo(), "/api/") !== 0) {
+            return;
+        }
+
+        // Je crée une nouvelle réponse mais en json ce coup ci
+        $response = new JsonResponse(
+            [
+                // je mets dedans le code http et le message d'erreur
+                "error" => $event->getThrowable()->getStatusCode(),
+                "message" => $event->getThrowable()->getMessage(),
+            ]
+        );
+
+        // on set la nouvelle réponse
+        $event->setResponse($response);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'kernel.exception' => 'onKernelException',
+        ];
+    }
+}
